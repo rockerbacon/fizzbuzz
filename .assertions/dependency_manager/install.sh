15faaffbf3a025ca421220b8ef7ce5a1ca5fe34e
rockerbacon <vas.vitor.santos@gmail.com>
2020-02-11 11:09:44 -0300

    Removed unecessary dependency

--- .assertions/dependency_manager/install.sh
+++ .assertions/dependency_manager/install.sh
@@ -3,4 +3,3 @@ source apt-package/install.sh cmake
 source apt-package/install.sh make
 source apt-package/install.sh g++
 source git/install.sh rockerbacon/assertions-test --local-only --version v2.2
-source git/install.sh rockerbacon/cpp-bash --local-only --version v1.1
